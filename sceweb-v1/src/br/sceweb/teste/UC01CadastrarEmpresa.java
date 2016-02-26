445d4f66cc0b9ad04c43500657832915e6b1f049
nfseixas <nataliafseixas@hotmail.com>
2016-02-26 15:27:02 -0300

    Comentário no caso de teste

--- sceweb-v1/src/br/sceweb/teste/UC01CadastrarEmpresa.java
+++ sceweb-v1/src/br/sceweb/teste/UC01CadastrarEmpresa.java
@@ -10,6 +10,9 @@ import org.junit.Test;
 import br.sceweb.model.Empresa;
 import br.sceweb.model.EmpresaDAO;
 
+/*
+ * Verificar o comportamento do sistema no cadastro de uma empresa
+ */
 
 public class UC01CadastrarEmpresa {
     static EmpresaDAO empresaDAO;
